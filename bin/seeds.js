13fd1258a3b91f812ee3d549c089e9a03dbe5e4b
javieramirez90 <javier.ramirez@aiesec.net>
2018-07-18 15:11:26 -0500

    Hero

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Recipe = require('../models/Recipe');
-const dbRecipe = 'proyecto2';
-mongoose.connect(`mongodb://localhost:27017/${dbRecipe}`);
+const dbRecipe = 'proyecto-2';
+mongoose.connect(`mongodb://Ricardo:25207481fuc@ds141671.mlab.com:41671/${dbRecipe}`);
 const recipes = [
   {
     photoURL: 'https://cdn.kiwilimon.com/recetaimagen/27113/th5-320x213-25233.jpg',
